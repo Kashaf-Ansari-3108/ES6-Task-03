a6f3a54f692702dfc5a36eb5f547f75f701d59e4
Kashaf Ansari <kashafansari3108@gmail.com>
2021-09-25 20:18:22 +0500

    work done

---
+++ app.js
@@ -0,0 +1,54 @@
+//1. write a program which shows the counter after each second.
+// let count = 0;
+// counter = () =>{
+//   count++;
+//    console.log(count);
+// }
+// setInterval(counter,1000);
+
+
+
+// 2. write a function which takes your name and displays the greeting with your name
+// let name = prompt("What's your Name?")
+// let greeting = (a) =>{
+// console.log(`Welcome ${a}!
+// How are You?`);
+// }
+// greeting(name);
+
+
+
+// 3. write a function(arrow function) which takes two values and return its sum as a result
+// let val1 = +prompt('Your 1st Value is:');
+// let val2 = +prompt('Your 2nd Value is:');
+// let sum = (a,b) =>{
+//  sumUp = `Your values are ${a} & ${b}
+//  and Sum is ${a+b}`
+//  return sumUp;
+// }
+//  let result = sum(val1,val2);
+//  console.log(result);
+
+
+
+
+//4. write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+// let num = +prompt("Enter a number");
+// let myFunction = (a) => num*0.5;
+// let result = myFunction(num);
+// console.log(result);
+
+
+//5. print simple array of [1,2,3,4,5] with the help of array map funtion
+// const arr = [1,2,3,4,5];
+// arr.map((i) => console.log(i));
+
+
+// 6. let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+// let arr = [{id:1,name:"abc"},{id:2,name:"efg"},{id:3,name:"hij"},{id:4,name:"xyz"}]
+// arr.map((i) => console.log(`id: ${i.id} 
+// name:${i.name}`));
+
+
+
